3a08dea72d8e7ba1c586dcb3cc3f92f9b7d2147c
EC2 Default User <ec2-user@ip-172-31-47-85.eu-west-1.compute.internal>
2020-03-24 11:35:27 +0000

    initial commit

--- src/demo.py
+++ src/demo.py
@@ -1,13 +1,25 @@
 import logging
-import sys 
+import sys
+import boto3
 
 logging.basicConfig(level=logging.INFO,
                     format='[%(asctime)s] {%(filename)s:%(lineno)d} %(levelname)s - %(message)s')
+bucketname='stuarts-bucket'
+itemname='test.txt'
+s3 = boto3.resource('s3')
+obj = s3.Object(bucketname, itemname)
 
-logging.info('Fat Lambda Params ****STARTED****')
+logging.info('Fat Lambda Params ***STARTED***')
 
-try:
-    logging.info(f'Fat Lambda started, param is: {sys.argv[1]}')
-except:
-    logging.info(f'Fat Lambda started but no input paramter supplied')
+logging.info(f'reading bucket {bucketname} file {itemname}')
+
+body = obj.get()['Body'].read()
+
+logging.info(f'{body}')
+
+
+#try:
+#    logging.info(f'Fat Lambda started, param is: {sys.argv[1]}')
+#except:
+#    logging.info(f'Fat Lambda started but no input paramter supplied')
 
